d21fc371afc4ac3f68c70d4a17ee5fd267652c0d
Vladimir Vertenz <vladmirworld@gmail.com>
2025-11-23 16:02:47 +0300

    feat: add frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPOSITORY,
+  DEPLOY_KEY = '~/.ssh/ya',
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      key: DEPLOY_KEY,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
