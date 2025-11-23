0952685250b2f02032d5463ace30469b2a2d48c9
Vladimir Vertenz <vladmirworld@gmail.com>
2025-11-23 16:14:00 +0300

    fix: frontend

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend && npm ci && npm run build',
     },
   },
 };
